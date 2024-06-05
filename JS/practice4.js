3d19e122d701b5e1fe524fae017d7091633816d2
yn130 <tjdpdms95@gmail.com>
2024-06-06 03:18:54 +0900

    공부하기

--- JS/practice4.js
+++ JS/practice4.js
@@ -10,33 +10,40 @@ const icecreams = [
   '뮤! 넌 내거야',
   '오레오 쿠키 앤 크림',
 ];
-// for (let i = 0; i <9; i++){
-//   const img = document.createElement("img");
-//   img.src = `/quiz_javascript_dom/js_dom_quiz/image/icecream${i+1}.png`;
-//   img.classList.add("image-box");
+for (let i = 0; i <icecreams.length; i++){
+  // const img = document.createElement("img");
+  // img.src = `/quiz_javascript_dom/js_dom_quiz/image/icecream${i+1}.png`;
+  // img.classList.add("image-box");
 
-//   const h3 = document.createElement("h3");
-//   h3.classList.add('text-center');
-//   h3.textContent = `Top${i + 1}`;
+  // const h3 = document.createElement("h3");
+  // h3.classList.add('text-center');
+  // h3.textContent = `Top${i + 1}`;
 
-//   const div = document.createElement("div");
-//   div.textContent = icecreams[i];
-//   div.classList.add("text-center");
+  // const div = document.createElement("div");
+  // div.textContent = icecreams[i];
+  // div.classList.add("text-center");
 
-//   const article = document.createElement('article');
-//   article.classList.add("article-box");
-//   article.appendChild(img);
-//   article.appendChild(h3);
-//   article.appendChild(div);
+  // const article = document.createElement('article');
+  // article.classList.add("article-box");
+  // article.appendChild(img);
+  // article.appendChild(h3);
+  // article.appendChild(div);
 
-//   document.querySelector("section").appendChild(article);
-// }
-
-icecreams.forEach((val, idx) => {
-    section.innerHTML += `
+  // document.querySelector("section").appendChild(article);
+  section.innerHTML += `
     <article class="article-box">
-    <img src = "/quiz_javascript_dom/js_dom_quiz/image/icecream${idx+1}.png" class=img-box" alt ="${val}">
-    <h3 class = 'text-center'>top${idx + 1}</h3>
-    <div class = "text-center">${val}</div>
-    </article>`
-});
+      <img src="/quiz_javascript_dom/js_dom_quiz/image/icecream${i+1}.png" class="image-box" alt="${icecreams[i]}">
+      <h3 class="text-center">Top${i + 1}</h3>
+      <div class="text-center">${icecreams[i]}</div>
+    </article>`;
+
+}
+
+// icecreams.forEach((val, idx) => {
+//     section.innerHTML += `
+//     <article class="article-box">
+//     <img src = "/quiz_javascript_dom/js_dom_quiz/image/icecream${idx+1}.png" class=img-box" alt ="${val}">
+//     <h3 class = 'text-center'>top${idx + 1}</h3>
+//     <div class = "text-center">${val}</div>
+//     </article>`
+// });
