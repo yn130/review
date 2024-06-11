d0d30bad1dc5ee665ab19eec674a4759799e66c3
yn130 <tjdpdms95@gmail.com>
2024-06-11 23:56:22 +0900

    node.js 서버 만들기

--- npm/4.server.js
+++ npm/4.server.js
@@ -1,32 +1,35 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer(function(req,res){
     let path = './view/'
     switch(req.url){
-        case '/' : 
+        case '/':
             path += 'index.html'
             res.statusCode = 200;
             break;
-        case '/about' :
+        case '/about':
             path += 'about.html'
             res.statusCode = 200;
             break;
-        default : 
+        default :
             res.statusCode = 404;
             break;
-        };
+    }
+    // fs.readFile(path, (err, data)=>{
+    //     if (err) throw err;
+    //     console.log(data);
+    // }) /// 에러 이유 찾기 
+    fs.readFile(path, (err,data)=> {
+        if(err){
+            console.log("err");
+            res.end();
 
-        fs.readFile(path, (err, data)=>{
-            if(err){
-                console.log('err');
-                res.end();
-            } else {
-                res.end(data);
-            }
+        }else {
+            res.end(data)
+        }
+    });
 
-        });
-
-}).listen(3000, function(){
+}).listen(3000,()=>{
     console.log('3000번 포트 실행 중');
-})
+});
